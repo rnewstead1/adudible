347755687185493d76614c6558e0404aa9f4185f
team.sonique.sns@googlemail.com <team.sonique.sns@googlemail.com@8df9a73d-d5eb-5009-e16d-d9b57fb914eb>
2011-04-21 14:36:33 +0000

    tweaking time selection screen, adding logos

--- trunk/gen/com/example/R.java
+++ trunk/gen/com/example/R.java
@@ -11,16 +11,19 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
-        public static final int wheel_bg=0x7f020001;
-        public static final int wheel_val=0x7f020002;
+        public static final int alarm_clock_ringing_trans=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int wheel_bg=0x7f020002;
+        public static final int wheel_val=0x7f020003;
     }
     public static final class id {
+        public static final int adudibleLogo=0x7f060007;
+        public static final int adudibleLogoText=0x7f060006;
         public static final int customLengthButton=0x7f060005;
         public static final int gridview=0x7f060001;
         public static final int introText=0x7f060000;
-        public static final int progressBar=0x7f060007;
-        public static final int timeLabel=0x7f060006;
+        public static final int progressBar=0x7f060009;
+        public static final int timeLabel=0x7f060008;
         public static final int wheelText=0x7f060002;
         public static final int wheel_mins=0x7f060003;
         public static final int wheel_secs=0x7f060004;
