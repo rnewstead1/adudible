ce463f696eb195f9a218871f7c56da097b997c29
team.sonique.sns@gmail.com <team.sonique.sns@gmail.com@8df9a73d-d5eb-5009-e16d-d9b57fb914eb>
2011-04-21 10:38:44 +0000

    Remove .idea dir

--- src/com/example/MonitorTimeActivity.java
+++ src/com/example/MonitorTimeActivity.java
@@ -2,14 +2,46 @@ package com.example;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MonitorTimeActivity extends Activity
 {
-    /** Called when the activity is first created. */
+    private long mStartTime;
+
+    private TextView mTimeLabel;
+    private Handler mHandler = new Handler();
+
+
+    private Runnable mUpdateTimeTask = new Runnable() {
+        public void run() {
+            mTimeLabel.setText("" + System.currentTimeMillis());
+            mHandler.postDelayed(mUpdateTimeTask, 100);
+        }
+    };
+
+
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.show_progress);
+
+//        mTimeLabel = (TextView) findViewById(R.id.timeLabel);
+//        findViewById(R.id.startButton).setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View view) {
+//                if (mStartTime == 0L) {
+//                    mStartTime = System.currentTimeMillis();
+//                    //mHandler.removeCallbacks(mUpdateTimeTask);
+//                    mHandler.postDelayed(mUpdateTimeTask, 100);
+//                }
+//            }
+//        });
+
     }
 }
