36ac0bf37282f5c9b478b77d22246efd77bcce6b
team.sonique.sns@googlemail.com <team.sonique.sns@googlemail.com@8df9a73d-d5eb-5009-e16d-d9b57fb914eb>
2011-04-21 09:52:46 +0000

    Added MonitorTimeActivity

---
+++ src/com/example/MonitorTimeActivity.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MonitorTimeActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
