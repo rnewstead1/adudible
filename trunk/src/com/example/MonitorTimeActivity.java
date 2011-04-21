8c13bf0164fd32ef1cdc41c3e0e4760ea9a632cc
team.sonique.sns@googlemail.com <team.sonique.sns@googlemail.com@8df9a73d-d5eb-5009-e16d-d9b57fb914eb>
2011-04-21 10:53:24 +0000

    Mark Chris: count down 5 seconds

--- trunk/src/com/example/MonitorTimeActivity.java
+++ trunk/src/com/example/MonitorTimeActivity.java
@@ -3,14 +3,13 @@ package com.example;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.SystemClock;
-import android.view.View;
-import android.widget.Button;
+import android.provider.Settings;
 import android.widget.TextView;
 
-public class MonitorTimeActivity extends Activity
-{
-    private long mStartTime;
+public class MonitorTimeActivity extends Activity {
+    
+    private long length = 5 * 1000;
+    private long endTime;
 
     private TextView mTimeLabel;
     private Handler mHandler = new Handler();
@@ -18,8 +17,13 @@ public class MonitorTimeActivity extends Activity
 
     private Runnable mUpdateTimeTask = new Runnable() {
         public void run() {
-            mTimeLabel.setText("" + System.currentTimeMillis());
-            mHandler.postDelayed(mUpdateTimeTask, 100);
+            if (System.currentTimeMillis() > endTime) {
+                mTimeLabel.setText("Get out of there");
+            }
+            else {
+                mTimeLabel.setText("" + (endTime - System.currentTimeMillis()));
+                mHandler.postDelayed(mUpdateTimeTask, 100);
+            }
         }
     };
 
@@ -33,15 +37,13 @@ public class MonitorTimeActivity extends Activity
         setContentView(R.layout.show_progress);
 
         mTimeLabel = (TextView) findViewById(R.id.timeLabel);
-        findViewById(R.id.startButton).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                if (mStartTime == 0L) {
-                    mStartTime = System.currentTimeMillis();
-                    //mHandler.removeCallbacks(mUpdateTimeTask);
-                    mHandler.postDelayed(mUpdateTimeTask, 100);
-                }
-            }
-        });
+    }
 
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        endTime = System.currentTimeMillis()+ length;
+        mHandler.postDelayed(mUpdateTimeTask, 100);
     }
 }
