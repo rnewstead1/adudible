847eac76ef7a9796b4c29e4180816a42157c89cf
team.sonique.sns@googlemail.com <team.sonique.sns@googlemail.com@8df9a73d-d5eb-5009-e16d-d9b57fb914eb>
2011-04-21 10:12:22 +0000

    Launching an activity [tw, rn]

--- trunk/src/com/example/SelectTimeActivity.java
+++ trunk/src/com/example/SelectTimeActivity.java
@@ -1,16 +1,17 @@
 package com.example;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class SelectTimeActivity extends Activity
-{
-    /** Called when the activity is first created. */
+public class SelectTimeActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_time);
 
@@ -18,7 +19,8 @@ public class SelectTimeActivity extends Activity
         final Button button = (Button) findViewById(R.id.button_id);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                setContentView(R.layout.show_progress);
+                Intent intent = new Intent(v.getContext(), MonitorTimeActivity.class);
+                startActivity(intent);
             }
         });
 
